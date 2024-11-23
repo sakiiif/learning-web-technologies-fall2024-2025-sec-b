c0755d32715bdac1631cfef470bc06aadc45a042
sakiiif <sakifullah787@gmail.com>
2024-11-23 11:50:19 +0600

    Add files via upload

---
+++ Lab 2/task1.php
@@ -0,0 +1,14 @@
+<?php
+
+$length = 20;
+$width = 5;
+
+$area = $length * $width;
+
+$perimetr = 2 * ($length + $width);
+echo "length: " . $length . " units<br>";
+echo "width: " . $width . " units<br>";
+echo "area: " . $area . " square units<br>";
+echo "perimeter: " . $perimetr. " units<br>";
+?>
+
